5dcf6e7291fcb79b8a161f3406cd30fbf3d8c35e
aakash07092004 <aakashmaithani0709@gmail.com>
2026-06-01 17:39:33 +0530

    Delete src/assests/main.jsx

--- src/assests/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import BlackEyeApp from './BlackEyeApp'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BlackEyeApp />
-  </React.StrictMode>,
-)
