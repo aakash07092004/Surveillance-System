bfca2804a6dd1af83bb55bc45c6027385c624cd7
aakash07092004 <aakashmaithani0709@gmail.com>
2026-06-01 17:38:10 +0530

    Add files via upload

---
+++ src/assests/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BlackEyeApp from './BlackEyeApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BlackEyeApp />
+  </React.StrictMode>,
+)
